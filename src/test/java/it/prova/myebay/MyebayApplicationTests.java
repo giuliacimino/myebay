ee0d81c3e9787b266ebf5b2cbd9873feb2f3f3d3
giuliacimino <giuliacimino00@gmail.com>
2023-05-11 17:46:24 +0200

    primo run del maven

---
+++ src/test/java/it/prova/myebay/MyebayApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.myebay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyebayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
